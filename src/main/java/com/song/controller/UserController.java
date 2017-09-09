fc174687fa3efa3ca5055a10cea05534b54b33c8
linsong <757803449@qq.com>
2017-09-09 16:34:38 +0800

    RequestMapping及Restful风格 post转换为put get delete

--- src/main/java/com/song/controller/UserController.java
+++ src/main/java/com/song/controller/UserController.java
@@ -1,17 +1,24 @@
 package com.song.controller;
 
-import com.song.service.IUserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping("/aaa")
 @Controller
 public class UserController {
 
-//    private IUserService userService;
-
-    @RequestMapping("/bbb")
-    public String toIndex(){
+    @RequestMapping(value = "/bbb/{id}",method = RequestMethod.GET)
+    public String toIndex(@PathVariable("id") Integer id){
         System.out.println("show User");
+        System.out.println(id);
+        return "success";
+    }
+
+    @RequestMapping(value="/restPut/{id}",method = RequestMethod.PUT)
+    public String restput(@PathVariable Integer id){
+        System.out.println(id);
         return "success";
     }
 }
